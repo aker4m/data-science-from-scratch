fc49a62648869948eeb16d1aab811518742d81ae
aker4m <mageaker@hotmail.com>
2018-08-14 14:00:55 +0900

    add code hypothesis_and_inference.py

--- hypothesis_and_inference.py
+++ hypothesis_and_inference.py
@@ -22,6 +22,73 @@ def normal_probability_outside(lo, hi, mu=0, sigma=1):
 
 #normal bounds
 
+def normal_upper_bound(probability, mu=0, sigma=1):
+    """returns the z for which P(Z <= z) = probability"""
+    return inverse_normal_cdf(probability, mu, sigma)
+
+def normal_lower_bound(probability, mu=0, sigma=1):
+    """returns the z for which P(Z >= z) = probability"""
+    return inverse_normal_cdf(1 - probability, mu, sigma)
+
+def normal_two_sided_bounds(probability, mu=0, sigma=1):
+    """returns the symmetric (about the mean) bounds
+        that contain the specified probability"""
+    tail_probability = ( 1 - probability) / 2
+    upper_bound = normal_lower_bound(tail_probability, mu, sigma)
+    lower_bound = normal_upper_bound(tail_probability, mu, sigma)
+    return lower_bound, upper_bound
+
+def two_sided_p_value(x, mu=0, sigma=1):
+    if x >= mu:
+        return 2 * normal_probability_above(x, mu, sigma)
+    else:
+        return 2 * normal_probability_below(x, mu, sigma)
+
+def count_extreme_values():
+    extreme_value_count = 0
+    for _ in range(100000):
+        num_heads = sum(1 if random.random() < 0.5 else 0
+                        for _ in range(1000))
+        if num_heads >= 530 or num_heads <= 470:
+            extreme_value_count += 1
+    return extreme_value_count / 100000
+
+upper_p_value = normal_probability_above
+lower_p_value = normal_probability_below
+
+#P-hacking
+
+def run_experiment():
+    """flip a fair coin 1000 times, True = heads, False = tails"""
+    return [random.random() < 0.5 for _ in range(1000)]
+
+def reject_fairness(experiment):
+    """using the 5% significance levels"""
+    num_heads = len([flip for flip in experiment if flip])
+    return num_heads < 469 or num_heads > 531
+
+#running an A/B test
+
+def estimated_parameters(N, n):
+    p = n / N
+    sigma = math.sqrt(p * (1 - p) / N)
+    return p, sigma
+
+def a_b_test_statistic(N_A, n_A, N_B, n_B):
+    p_A, sigma_A = estimated_parameters(N_A, n_A)
+    p_B, sigma_B = estimated_parameters(N_B, n_B)
+    return (p_B - p_A) / math.sqrt(sigma_A ** 2 + sigma_B ** 2)
+
+#Bayesian Inference
+
+def B(alpha, beta):
+    """a normalizing constant so that the total probability is 1"""
+    return math.gamma(alpha) * math.gamma(beta) / math.gamma(alpha + beta)
+
+def beta_pdf(x, alpha, beta):
+    if x < 0 or x > 1:
+        return 0
+    return x ** (alpha - 1)* (1 - X) ** (beta - 1) / B(alpha, beta)
 
 if __name__ == '__main__':
     mu_0, sigma_0 = normal_approximation_to_binomial(1000, 0.5)
